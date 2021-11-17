a8568e07f0e23392e6fd923e11e12b67364d4ecc
Sukyeong Park <zztnrudzz123433@gmail.com>
2021-11-17 04:58:43 +0000

    Update MyAccount and Routing Transfer Page

--- src/components/ChildrenAccountTab.js
+++ src/components/ChildrenAccountTab.js
@@ -12,11 +12,59 @@ const AccountTab = styled.TouchableOpacity`
   box-shadow: 0px 7px 8px rgba(100, 100, 111, 0.2) ;
 `;
 
+// 자녀 이름 컨테이너
+const ChildrenNameContainer = styled.View`
+  /* background-color: blueviolet; */
+  width: 100%;
+  height: 20%;
+`;
+
+
+// 화폐 단위 컨테이너
+const MoneyUnitContainer = styled.View`
+  /* background-color: chartreuse; */
+  width: 100%;
+  height: 25%;
+`;
+
+
+// 자녀 잔액 컨테이너
+const ChildrenMoneyContainer = styled.View`
+  /* background-color: coral; */
+  width: 100%;
+  height: 27%;
+`;
+
+
+// 송금하기 버튼 컨테이너
+const TransferTabContainer = styled.View`
+  /* background-color: cornsilk; */
+  width: 100%;
+  height: 28%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TransferTab = styled.TouchableOpacity`
+  background-color: #00ac84;
+  width: 85%;
+  height: 65%;
+  border-radius: 10px;
+`;
+
 export default function ChildrenAccountTab() {
   const navigation = useNavigation()
 
   return (
       <AccountTab onPress={() => {navigation.navigate('ChildMain')}}>
+        <ChildrenNameContainer></ChildrenNameContainer>
+        <MoneyUnitContainer></MoneyUnitContainer>
+        <ChildrenMoneyContainer></ChildrenMoneyContainer>
+        <TransferTabContainer>
+          <TransferTab onPress={() => navigation.navigate('Transfer')}>
+
+          </TransferTab>
+        </TransferTabContainer>
       </AccountTab>
   )
 }
--- src/components/MyAccountContainer.js
+++ src/components/MyAccountContainer.js
@@ -9,8 +9,58 @@ const MyAccountContainerBox = styled.TouchableOpacity`
   width: 80%;
   height: 18%;
   position: absolute;
+  display: flex;
+  flex-direction: column;
   border-radius: 15px;
   box-shadow: 0px 7px 15px rgba(100, 100, 111, 0.2) ;
+`;
+
+// 내 계좌 제목 컨테이너 + 새로고침 버튼
+const MyAccountHeaderContainer = styled.View`
+  /* background-color: antiquewhite; */
+  width: 100%;
+  height: 35%;
+  justify-content: center;
+  padding: 0px 15px;
+`;
+
+const MyAccountTitle = styled.Text`
+  color: #BAB9B9;
+  font-size: 15px;
+`;
+
+const MyMoneyContainer = styled.View`
+  /* background-color: aqua; */
+  width: 100%;
+  height: 30%;
+  justify-content: center;
+  padding: 0px 25px;
+`;
+
+const MyMoney = styled.Text`
+  color: #6E6E6E;
+  font-size: 40px;
+`;
+
+const MyAccountInfoContainer = styled.View`
+  /* background-color: bisque; */
+  width: 100%;
+  height: 35%;
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  padding: 0px 15px;
+`;
+
+const BankName = styled.Text`
+  color: #BAB9B9;
+  font-size: 14px;
+  margin-right: 5px;
+`;
+
+const AccountNumber = styled.Text`
+  color: #6E6E6E;
+  font-size: 14px;
 `;
 
 export default function MyAccountContainer() {
@@ -18,7 +68,16 @@ export default function MyAccountContainer() {
 
   return (
     <MyAccountContainerBox onPress={() => navigation.navigate('MyAccountDetails')}>
-
+      <MyAccountHeaderContainer>
+        <MyAccountTitle>내 계좌</MyAccountTitle>
+      </MyAccountHeaderContainer>
+      <MyMoneyContainer>
+        <MyMoney>130,000</MyMoney>
+      </MyMoneyContainer>
+      <MyAccountInfoContainer>
+        <BankName>농협</BankName>
+        <AccountNumber>44580259645844</AccountNumber>
+      </MyAccountInfoContainer>
     </MyAccountContainerBox>
   )
 }
